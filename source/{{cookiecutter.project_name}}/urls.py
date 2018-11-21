87db2bf9ca0bea19433e034539c6dfd3249828c7
apecnascimento <apecnascimento@gmail.com>
2018-11-21 19:55:21 -0300

    Add rest_framework_swagger.

--- source/{{cookiecutter.project_name}}/urls.py
+++ source/{{cookiecutter.project_name}}/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework_swagger.views import get_swagger_view
+
+swagger_view = get_swagger_view(title='{{cookiecutter.project_name}}')
 
 urlpatterns = [
+    path('/', swagger_view)
     path('admin/', admin.site.urls),
 ]
