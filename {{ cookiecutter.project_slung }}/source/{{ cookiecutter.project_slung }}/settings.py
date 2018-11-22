cea278ff5a2cf70615cec9df5d6a9450e81d633b
apecnascimento <apecnascimento@gmail.com>
2018-11-22 18:40:58 -0300

    Add TIME_ZONE  and LANGUAGE_CODE  template variable.

--- {{ cookiecutter.project_slung }}/source/{{ cookiecutter.project_slung }}/settings.py
+++ {{ cookiecutter.project_slung }}/source/{{ cookiecutter.project_slung }}/settings.py
@@ -99,9 +99,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = '{{ cookiecotter.LANGUAGE_CODE }}'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = '{{cookiecutter.TIME_ZONE}}'
 
 USE_I18N = True
 
