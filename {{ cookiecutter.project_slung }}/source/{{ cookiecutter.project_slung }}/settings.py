7bd25ee60a5424c6b3d5cf9f91d430a0f21d674c
apecnascimento <apecnascimento@gmail.com>
2018-11-22 22:47:53 -0300

    Add python-decouple.

--- {{ cookiecutter.project_slung }}/source/{{ cookiecutter.project_slung }}/settings.py
+++ {{ cookiecutter.project_slung }}/source/{{ cookiecutter.project_slung }}/settings.py
@@ -1,4 +1,5 @@
 import os
+from decouple import Config, Csv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -8,12 +9,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = Config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ['DJANGO_DEBUG']
+DEBUG = Config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS'].split(',')
+ALLOWED_HOSTS = Config(ALLOWED_HOSTS, cast=Csv())
 
 
 # Application definition
